08bb3d777f649fcd301f240405721787739e428c
Navneet <148934112+Navneet0094@users.noreply.github.com>
2024-04-11 18:07:59 +0530

    Delete loops directory

--- loops/for/2nd.py
+++
@@ -1,9 +0,0 @@
-# for i in range(6):
-#     print(i+1)
-
-
-# for j in range(1,6):
-#     print(j )
-    
-for k in range(1,10,2):
-    print(k)
--- loops/for/5 table.py
+++
@@ -1,5 +0,0 @@
-for i in range(1,12):
-    if(i==11):
-        break
-    print("5 x ",i,"=",5*i)
-print("loop se nikal gya")
