dcb90cdafe132db3be25df41651e89fdae474a3c
Navneet <148934112+Navneet0094@users.noreply.github.com>
2024-04-11 18:07:19 +0530

    Delete function directory

--- function/area.py
+++
@@ -1,4 +0,0 @@
-def area(a):
-    return 3.14*a*a
-a= int(input("enter a "))
-print(area(a))
--- function/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-def average(a,b,c=1):
-#     print("the average is ",(a+b+c)/2)
-# #if we give average(b=1) it will give us error as a is not passed
-# average(1,2)
