48d7f1df852a2fee374d148c8bcb680365d12629
Navneet <148934112+Navneet0094@users.noreply.github.com>
2024-04-12 22:57:47 +0530

    Add files via upload

---
+++ Loops/While/1.py
@@ -0,0 +1,13 @@
+# i= int(input("enter number"))
+# while(i<=38):
+#     i=int(input("enter nu"))
+#     print(i)
+
+# print("done with the loop")
+count =5
+while(count >0):
+    print(count)
+    count-=1
+else:
+    print("i am inside else1")
+    
