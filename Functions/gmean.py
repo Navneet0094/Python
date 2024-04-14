f050c43a2ee2ec79a62d31575fbeb13713be7c1d
Navneet <148934112+Navneet0094@users.noreply.github.com>
2024-04-14 23:13:07 +0530

    Add files via upload

---
+++ Functions/gmean.py
@@ -0,0 +1,16 @@
+def calculategmean(a,b):
+    mean=(a*b)/(a+b)
+    print(mean)
+def greater(f,g):
+    if(a>b):
+        print("1st number is greater ")
+    else:
+        print("2nd number is greater")
+a=8
+b=9
+calculategmean(a,b)
+greater(a,b)
+c=1
+d=11
+calculategmean(c,d)
+greater(c,d)
